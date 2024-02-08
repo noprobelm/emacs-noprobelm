0f7f82033bb25be83cc7768d0e455085c40ffa6f
noprobelm <noprobelm@protonmail.com>
2024-02-08 11:41:38 -0500

    Replaced 'company' with 'corfu'; added 'kind-icon' package

--- modules/np-completion.el
+++ modules/np-completion.el
@@ -5,9 +5,22 @@
   :config
   (helm-mode 1))
 
-(use-package company
-  :config
-  (add-hook 'after-init-hook 'global-company-mode))
+(use-package corfu
+  :elpaca (:files (:defaults "extensions/corfu-info.el" "extensions/corfu-history.el" "extensions/corfu-popupinfo.el"))
+  :hook
+  (prog-mode . corfu-mode)
+  (text-mode . corfu-mode)
+  :init
+  (setq corfu-auto t)
+  (setq corfu-auto-delay 0.2)
+  (setq corfu-auto-prefix 1)
+  (setq corfu-popupinfo-delay 0.5))
+
+(use-package kind-icon
+  :after corfu
+  :init
+  (setq kind-icon-default-face 'corfu-default)
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
 (provide 'np-completion)
 ;;; np-completion.el ends here.
