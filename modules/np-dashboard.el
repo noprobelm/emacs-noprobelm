2009925ee89729c432724934cf0b4f12af86ebfb
noprobelm <noprobelm@protonmail.com>
2024-02-08 13:40:07 -0500

    Corrected spelling of dashboard-items in dashbaord

--- modules/np-dashboard.el
+++ modules/np-dashboard.el
@@ -18,7 +18,7 @@
    dashboard-set-file-icons t
    dashboard-center-content t
    dashboard-set-navigator t
-   dashbaord-items '((recents  . 5)
+   dashboard-items '((recents  . 5)
                      (bookmarks . 5)
 		     (projects . 5)
 		     (agenda . 5)))
