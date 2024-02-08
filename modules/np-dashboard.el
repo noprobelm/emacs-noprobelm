c9d0675294ab483636a7294f30a206882accdea4
noprobelm <noprobelm@protonmail.com>
2024-02-08 12:05:36 -0500

    Cleaned up dashboard; added new banner

--- modules/np-dashboard.el
+++ modules/np-dashboard.el
@@ -6,36 +6,35 @@
   :init
   ;; Setup post initialization dashboard hooks.
   (dashboard-setup-startup-hook)
-	(setq dashboard-banner-logo-title
-	      "Noprobelm's Dashboard\ntest")
-	(setq dashboard-banner-logo-title
-	      (format "Emacs ready in %.2f seconds with %d garbage collections."
-		      (float-time (time-subtract after-init-time before-init-time)) gcs-done))
-	(setq dashboard-startup-banner "~/.emacs.noprobelm/images/volo.png")
-	(setq dashboard-image-banner-max-height 600)
-	(setq dashboard-image-banner-max-width 900)
-	(setq dashboard-items '((recents  . 5)
-				(bookmarks . 5)
-				(projects . 5)
-				(agenda . 5)
-				))
 
-(setq dashboard-set-heading-icons t)
-(setq dashboard-set-file-icons t)
-(setq dashboard-center-content t)
+  (setq
+   dashboard-banner-logo-title
+   (format "Emacs ready in %.2f seconds with %d garbage collections."
+           (float-time (time-subtract after-init-time before-init-time)) gcs-done)
+   dashboard-startup-banner "~/.emacs.noprobelm/banners/ue-colorful.png"
+   dashboard-image-banner-max-height 600
+   dashboard-image-banner-max-width 900
+   dashboard-set-heading-icons t
+   dashboard-set-file-icons t
+   dashboard-center-content t
+   dashboard-set-navigator t
+   dashbaord-items '((recents  . 5)
+                     (bookmarks . 5)
+		     (projects . 5)
+		     (agenda . 5)))
+
+  (setq initial-buffer-choice #'dashboard-open)
 
   (defun dashboard-refresh-buffer-silent ()
     "Refresh buffer in background."
     (interactive)
-    (let ((dashboard-force-refresh t)) (dashboard-insert-startupify-lists)))
+    (let ((dashboard-force-refresh t))
+      (dashboard-insert-startupify-lists)))
 
   (defun dashboard-switch ()
-    (interactive)
     "Switch to dashboard buffer"
-    (switch-to-buffer dashboard-buffer-name))
-
-  (setq dashboard-set-navigator t)
-  (setq initial-buffer-choice #'dashboard-open))
+    (interactive)
+    (switch-to-buffer dashboard-buffer-name)))
 
 (provide 'np-dashboard)
 ;;; np-dashboard.el ends here.
