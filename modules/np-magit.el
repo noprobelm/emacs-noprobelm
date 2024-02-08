835b493edbc73a6d89e2a0f36faa9778e162f034
noprobelm <noprobelm@protonmail.com>
2024-02-08 11:19:13 -0500

    Added package diff-hl

--- modules/np-magit.el
+++ modules/np-magit.el
@@ -3,5 +3,12 @@
 ;;; Code:
 (use-package magit)
 
+(use-package diff-hl
+  :hook
+  (prog-mode . diff-hl-mode)
+  (prog-mode . diff-hl-flydiff-mode)
+  (prog-mode . diff-hl-show-hunk-mouse-mode)
+  (window-state-change . diff-hl-update))
+
 (provide 'np-magit)
 ;;; np-magit.el ends here.
