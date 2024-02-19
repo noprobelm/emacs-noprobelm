ca6ce2396d44e2b0c52c2d7c09340fe38a1a337d
noprobelm <noprobelm@protonmail.com>
2024-02-19 12:34:32 -0500

    Moved hooks to :hook keyword in smartparens module

--- modules/np-smartparens.el
+++ modules/np-smartparens.el
@@ -2,11 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 (use-package smartparens
+  :hook ((prog-mode . smartparens-mode)
+         (text-mode . smartparens-mode))
   :config
   (require 'smartparens-config)
-  (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
-  (add-hook 'prog-mode-hook #'smartparens-mode)
-  (add-hook 'text-mode-hook #'smartparens-mode))
+  (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil))
 
 (provide 'np-smartparens)
 ;;; np-smartparens.el ends here.
