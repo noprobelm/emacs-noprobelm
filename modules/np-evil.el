f1a7ee297a84bddc07e78104a2341ecd1204dc3b
noprobelm <noprobelm@protonmail.com>
2024-02-19 12:27:20 -0500

    Formatting updates to np-evil module

--- modules/np-evil.el
+++ modules/np-evil.el
@@ -5,18 +5,14 @@
   :after general
   :init
   (setq evil-want-integration t)
-  ;; This is optional since it's already set to t by default.
   (setq evil-want-keybinding nil)
   :config
   (evil-mode 1)
-  (evil-set-undo-system 'undo-fu)
-  ;; Window keybinds
-  )
+  (evil-set-undo-system 'undo-fu))
 
 (use-package evil-collection
   :init (evil-collection-init)
-  :custom (evil-collection-setup-minibuffer t) ;; Enables evil in the minifbuffer
-)
+  :custom (evil-collection-setup-minibuffer t))
 
 (provide 'np-evil)
 ;;; np-evil.el ends here.
