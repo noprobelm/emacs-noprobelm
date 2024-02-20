87379a3be765931e60235893e249174de4d44b55
noprobelm <noprobelm@protonmail.com>
2024-02-20 13:42:42 -0500

    Removed redundant/unused custom faces from custom.el

--- custom.el
+++ custom.el
@@ -5,17 +5,4 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100"
-     default))
- '(helm-minibuffer-history-key "M-p"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:height 180 :extend t :inherit outline-1))))
- '(org-level-2 ((t (:height 170 :extend t :inherit outline-2))))
- '(org-level-3 ((t (:height 160 :extend t :inherit outline-3))))
- '(org-level-4 ((t (:height 150 :extend t :inherit outline-4))))
- '(org-level-5 ((t (:height 150 :extend t :inherit outline-5))))
- '(org-level-6 ((t (:height 140 :extend t :inherit outline-6))))
- '(org-level-7 ((t (:height 130 :extend t :inherit outline-7)))))
+     default)))
