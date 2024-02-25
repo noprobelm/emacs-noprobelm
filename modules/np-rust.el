bbf1eb025e4bddff8d8802f5785b159c0725558a
noprobelm <noprobelm@protonmail.com>
2024-02-25 14:21:43 -0500

    Added function np/rustic-docstring-front-page to rustic config

--- modules/np-rust.el
+++ modules/np-rust.el
@@ -5,6 +5,12 @@
   :after corfu
   :mode ("\\.rs\\'" . rustic-mode)
   :init
+  (defun np/rustic-docstring-front-page ()
+    "Use `comment-dwim' to make a docstring."
+  (interactive)
+  (let ((comment-start "//! "))
+    (call-interactively 'comment-dwim)))
+
   (setq rustic-lsp-client 'eglot))
 
 (provide 'np-rust)
